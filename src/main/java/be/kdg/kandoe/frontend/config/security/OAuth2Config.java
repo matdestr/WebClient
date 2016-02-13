ece75c6d24b04ec6307d79558514300728ae30f0
Jelle Spoelders <jelle.spoelders@student.kdg.be>
2016-02-13 14:00:18 +0100

    Added documentation to OAuth2Config

--- src/main/java/be/kdg/kandoe/frontend/config/security/OAuth2Config.java
+++ src/main/java/be/kdg/kandoe/frontend/config/security/OAuth2Config.java
@@ -5,13 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+/**
+ * This class configures the authorization server for use of OAuth 2.0
+ * */
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
@@ -20,25 +25,39 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private OAuthClientDetailsService oAuthClientDetailsService;
-
+    
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         return new JwtAccessTokenConverter();
     }
 
+    /*@Bean
+    public JwtTokenStore jwtTokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }*/
+
+    /**
+     * Defines the security constraints on the token endpoint
+     * */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')").checkTokenAccess(
                 "permitAll()");
     }
 
+    /**
+     * Defines the authorization and token endpoints and the token services
+     * */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        //endpoints.tokenStore(jwtTokenStore()); // TODO : Check
         endpoints.authenticationManager(authenticationManager).accessTokenConverter(accessTokenConverter());
     }
 
-
-
+    /**
+     * Defines the client details service
+     * */
+    // Clients can be initialized, or you can refer to an existing store
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         // @formatter:off
