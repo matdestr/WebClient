d2944cb004394731839aa7bdbf90e01b452112bf
DevotedPie <notvalid@nomail.com>
2016-02-19 14:13:45 +0100

    Fixed login issue

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,5 +1,5 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './components/app.component'
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './components/app.component';
 import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from "angular2/router";
 import {Http, HTTP_PROVIDERS} from "angular2/http";
 import {provide} from "angular2/core";
