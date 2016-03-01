f813d6bde14422800f0e147dcdd511b722a36468
vincentpolfliet <vincent.polfliet@student.kdg.be>
2016-03-01 22:07:00 +0100

    websockets

---
+++ src/main/java/be/kdg/kandoe/frontend/controller/websockets/WebSocketController.java
@@ -0,0 +1,5 @@
+package be.kdg.kandoe.frontend.controller.websockets;
+
+public interface WebSocketController {
+    String[] getEndpoints();
+}
---
+++ src/test/java/integrationtest/websockets/ITestWebSocketConfiguration.java
@@ -0,0 +1,55 @@
+package integrationtest.websockets;
+
+import be.kdg.kandoe.frontend.config.RootContextConfig;
+import be.kdg.kandoe.frontend.config.WebContextConfig;
+import integrationtest.OAuth.OAuth2TestConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RootContextConfig.class, WebContextConfig.class, WebSocketTestConfig.class})
+@WebAppConfiguration
+public class ITestWebSocketConfiguration {
+    private WebSocketClient webSocketClient;
+    private WebSocketStompClient stompClient;
+    private String baseUrl;
+
+    @Before
+    public void setUp(){
+        baseUrl = String.format("ws://%s/",System.getProperty("test.baseUrl") );
+
+        webSocketClient = new StandardWebSocketClient();
+        stompClient = new WebSocketStompClient(webSocketClient);
+        stompClient.setMessageConverter(new StringMessageConverter());
+    }
+
+    @Test
+    public void testWebsocketsWithStomp(){
+        String url = baseUrl + "test";
+        StompSessionHandler handler = new StompSessionHandlerAdapter() {
+            @Override
+            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                System.out.println("connected");
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                System.out.println(payload.toString());
+                super.handleFrame(headers, payload);
+            }
+        };
+        stompClient.connect(url, handler);
+    }
+}
