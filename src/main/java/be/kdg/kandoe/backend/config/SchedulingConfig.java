4e00b221f395312c6c4590d39601ccc88e66361b
vincentpolfliet <vincent.polfliet@student.kdg.be>
2016-03-06 14:42:06 +0100

    tasks

--- src/main/java/be/kdg/kandoe/backend/config/SchedulingConfig.java
+++ src/main/java/be/kdg/kandoe/backend/config/SchedulingConfig.java
@@ -1,12 +1,37 @@
 package be.kdg.kandoe.backend.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
-public class SchedulingConfig {
+public class SchedulingConfig implements AsyncConfigurer {
 
+    @Bean
+    public TaskScheduler taskScheduler(){
+        return new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor());
+    }
+
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
 }
