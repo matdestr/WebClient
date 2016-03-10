433a6038bf1956a07050f0460d9d99d02f85ac89
DevotedPie <notvalid@nomail.com>
2016-03-10 19:31:02 +0100

    chat

--- src/main/java/be/kdg/kandoe/frontend/controller/resources/sessions/chat/ChatMessageResource.java
+++ src/main/java/be/kdg/kandoe/frontend/controller/resources/sessions/chat/ChatMessageResource.java
@@ -1,13 +1,13 @@
 package be.kdg.kandoe.frontend.controller.resources.sessions.chat;
 
-import be.kdg.kandoe.backend.model.users.User;
+import be.kdg.kandoe.frontend.controller.resources.users.UserResource;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class ChatMessageResource {
-    private User user;
+    private UserResource user;
     private String message;
     private Date date;
 }
